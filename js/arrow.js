2b1fa6062f705901a520c7469dd20971fb34ccb7
thanhha-loz <nguyenthanhha.tai.555@gmail.com>
2020-10-04 21:28:00 +0700

    Update arrow.js

--- js/arrow.js
+++ js/arrow.js
@@ -1,1 +1,14 @@
-$("#arrow").focus(() => {alert( "Handler for .focus() called." );$("#arrow").addClass("animated");});
+$( document ).ready(() => {
+    console.log('dope');
+    $("#name").focus(() => {
+    console.log( "Handler for .focus() called." );
+    $("#arrow").removeClass("arrow");
+    $("#arrow").addClass("animated");
+});
+$("#name").blur(() => {
+    console.log( "Handler for .focus() called." );
+    $("#arrow").removeClass("animated");
+    $("#arrow").addClass("arrow");
+})
+}
+)
