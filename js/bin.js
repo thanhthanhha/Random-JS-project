39ec905fc2a8b52ffedcb37d38bc86e5994e38cb
thanhha-loz <nguyenthanhha.tai.555@gmail.com>
2020-10-05 19:06:58 +0700

    Update bin.js

--- js/bin.js
+++ js/bin.js
@@ -10,6 +10,49 @@ $("#name").blur(() => {
     console.log( "Handler for .focus() called." );
     $("#arrow").removeClass("animated");
     $("#arrow").addClass("arrow");
-})
+});
 }
 )
+function showTime() {
+    var today = new Date(),
+    hour = today.getHours(),
+    min = today.getMinutes(),
+    sec = today.getSeconds();
+
+    const amPm = hour >= 12 ? 'AM' : 'PM';
+
+    hour = hour % 12 || 12;
+    
+    $('#time').html(`${addZero(hour)}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}`);
+    setTimeout(showTime, 1000);
+}
+function addZero(n) {
+    return(parseInt(n, 10) < 10 ? '0' : '') + n;
+}
+
+//get local name
+function getName() {
+    if (localStorage.getItem('name') === null ) {
+        $('#name').html('thanhha');
+    } else {
+        $('#name').html(localStorage.getItem('name'));
+        console.log(localStorage.getItem('name'));
+    }
+}
+jQuery(document.body).on("blur", () => {
+    $('#name').blur(() => {
+        localStorage.setItem('#name', e.target.innerText);
+    });
+})
+jQuery(document.body).on("click", () => {
+    $('#name').keypress(() => {
+        if(e.which == 13 || e.keyCode == 13) {
+            localStorage.setItem('#name', e.target.innerText);
+            $('#name').blur();
+        }
+        console.log('asdsadasd');
+    })
+})
+//run
+showTime();
+getName();
